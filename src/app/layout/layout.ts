893b17c5fe6ca76352504f63ae79a17700b15e19
Anderson <andersonribeiro.si@hotmail.com>
2025-08-13 18:24:13 -0300

    feat(layout): adicionar funcionalidade de logout
    - Injetado AuthService e Router no MainLayoutComponent
    - Criado método `logout()` que remove o token e redireciona para a tela de login
    - Alterado HTML do layout para chamar `logout()` ao clicar no botão "Sair"

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet, RouterLink } from '@angular/router';
+import { RouterModule, RouterOutlet, RouterLink, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -9,4 +10,14 @@ import { RouterModule, RouterOutlet, RouterLink } from '@angular/router';
   templateUrl: './layout.html',
   styleUrls: ['./layout.scss'],
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']); // redireciona para a tela de login
+  }
+}
