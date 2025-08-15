250d218c873324c002ea06507b838fdb80091d05
Anderson <andersonribeiro.si@hotmail.com>
2025-08-15 11:26:39 -0300

    feat(specialties): criação de formulário e listagem de especialidades
    - Criado SpecialtiesFormComponent para cadastro e edição de especialidades
    - Criado SpecialtiesListComponent para listagem de especialidades
    - Adicionadas rotas para navegação:
        - /specialties → lista de especialidades
        - /specialties/new → formulário de cadastro
        - /specialties/edit/:id → formulário de edição
    - Estrutura standalone do Angular 17 aplicada nos componentes

--- src/app/components/specialties/specialties-form/specialties-form.ts
+++ src/app/components/specialties/specialties-form/specialties-form.ts
@@ -1,11 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Specialty } from '../../../models/specialty';
+import { specialtiesService } from '../../../services/specialties.service';
 
 @Component({
   selector: 'app-specialties-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
   templateUrl: './specialties-form.html',
   styleUrl: './specialties-form.scss'
 })
-export class SpecialtiesForm {
+export class SpecialtiesForm implements OnInit {
+  specialtyId?: number;
+  loading = false;
+  errorMessage = '';
 
+  specialtyForm!: FormGroup;
+
+  constructor(
+    private specialtiesService: specialtiesService,
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.specialtyForm = this.fb.group({
+      name: ['', Validators.required]
+    })
+
+    this.specialtyId = Number(this.route.snapshot.paramMap.get('id'))
+
+    if (this.specialtyId) {
+      this.loadSpecialty(this.specialtyId)
+    }
+  }
+
+  loadSpecialty(id: number): void {
+    this.loading = true;
+    this.errorMessage = '';
+    this.specialtiesService.getById(id).subscribe({
+      next: (data) => {
+        const specialty: Specialty = {
+          ...data,
+          name: data.name
+        };
+
+        this.specialtyForm.patchValue({
+          name: specialty.name
+        })
+
+        this.loading = false
+      },
+      error: () => {
+        this.errorMessage = 'Erro ao carregar a especialidade';
+        this.loading = false;
+      }
+    })
+  }
+
+  private mapFormToPatient(): Specialty {
+    const form = this.specialtyForm.value;
+    return {
+      name: form.name,
+    };
+  }
+
+  onSubmit(): void {
+    if (this.specialtyForm.invalid) return;
+
+    this.loading = true;
+    this.errorMessage = '';
+
+    const patientData: Specialty = this.mapFormToPatient();
+
+    if (this.specialtyId) {
+      this.specialtiesService.update(this.specialtyId, patientData).subscribe({
+        next: () => {
+          this.loading = false;
+          this.router.navigate(['/specialties']);
+        },
+        error: () => {
+          this.errorMessage = 'Erro ao atualizar a especialidade';
+          this.loading = false;
+        },
+      });
+    } else {
+      this.specialtiesService.create(patientData).subscribe({
+        next: () => {
+          this.loading = false;
+          this.router.navigate(['/specialties']);
+        },
+        error: () => {
+          this.errorMessage = 'Erro ao criar especialidade';
+          this.loading = false;
+        },
+      });
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/specialties']);
+  }
 }
--- src/app/components/specialties/specialties-list/specialties-list.ts
+++ src/app/components/specialties/specialties-list/specialties-list.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Specialty } from '../../../models/specialty';
+import { specialtiesService } from '../../../services/specialties.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-specialties-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './specialties-list.html',
-  styleUrl: './specialties-list.scss'
+  styleUrl: './specialties-list.scss',
 })
-export class SpecialtiesList {
+export class SpecialtiesList implements OnInit {
+  specialties?: Specialty[]
+  loading = false;
+  errorMessage = '';
 
+  constructor(
+    private specialtiesService: specialtiesService
+  ) { }
+
+  ngOnInit(): void {
+    this.loadSpecialties()
+  }
+
+  loadSpecialties() {
+    this.loading = true;
+    this.errorMessage = '';
+
+    this.specialtiesService.getAll().subscribe({
+      next: (specialties) => {
+        this.specialties = specialties
+        this.loading = false
+      },
+      error: (err) => {
+        this.errorMessage = 'Erro ao carregar pacientes';
+        this.loading = false;
+      }
+    })
+  }
+
+  deleteSpecialty(id: number): void {
+
+    if (!id) return
+    if (!confirm('Deseja realmente deletar este paciente?')) return;
+
+    this.specialtiesService.delete(id).subscribe({
+      next: () => this.loadSpecialties(),
+      error: () => alert('Erro ao deletar a especialidade'),
+    })
+  }
 }
